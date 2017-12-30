118da4865f37d7b8213732ea2f55ebf17eaf121f
Anna Wawrowska <anna.wawrowska.it@gmail.com>
2017-12-30 17:10:45 +0100

    Add slider elements and style animation

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,28 +1,31 @@
 
-// $(document).ready(function() {
-//   $('.allOffers').slick({
-//     centerMode: true,
-//    centerPadding: '60px',
-//   slidesToShow: 3,
-//   // responsive: [
-//   //   {
-//   //     breakpoint: 768,
-//   //     settings: {
-//   //       arrows: false,
-//   //       centerMode: true,
-//   //       centerPadding: '40px',
-//   //       slidesToShow: 3
-//   //     }
-//   //   },
-//   //   {
-//   //     breakpoint: 480,
-//   //     settings: {
-//   //       arrows: false,
-//   //       centerMode: true,
-//   //       centerPadding: '40px',
-//   //       slidesToShow: 1
-//   //     }
-//   //   }
-//   // ]
-//   });
-// })
+$(document).ready(function() {
+  $('.allOffers').slick({
+    centerMode: true,
+    slidesToShow: 3,
+    variableWidth: true,
+    prevArrow: '.slider__arrow--prev',
+    nextArrow: '.slider__arrow--next',
+    swipe: false
+    // responsive: [
+    //   {
+    //     breakpoint: 768,
+    //     settings: {
+    //       arrows: false,
+    //       centerMode: true,
+    //       centerPadding: '40px',
+    //       slidesToShow: 3
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 480,
+    //     settings: {
+    //       arrows: false,
+    //       centerMode: true,
+    //       centerPadding: '40px',
+    //       slidesToShow: 1
+    //     }
+    //   }
+    // ]
+  });
+})
