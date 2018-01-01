3e2f6d617ec317702701c834aad97b0ccf99a78a
Anna Wawrowska <anna.wawrowska.it@gmail.com>
2018-01-02 00:03:40 +0100

    Add register function (POST)

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -2,6 +2,8 @@
 (function () {
   $(document).ready(function() {
     project.sliderInit();
+    project.auth.addEventsListeners();
+
   });
 
   var project = {
@@ -26,6 +28,40 @@
           },
         ]
       });
+    },
+    auth: {
+      addEventsListeners: function() {
+        $('#formSignUp').submit(function (event) {
+          event.preventDefault();
+          var email = $('#signUpEmail')[0].value;
+          var password = $('#signUpPassword')[0].value;
+          project.auth.register(email, password);
+        })
+      },
+      register: function(email, password) {
+        console.log(email);
+        console.log(password);
+        $.ajax({
+          url: 'https://foodsi-stage.applover.pl/api/v1/auth',
+          type: 'POST',
+          dataType: 'json',
+          data: {
+            "email": email,
+            "password": password
+          },
+          success: function(data) {
+            console.log('success');
+            console.log(data);
+          },
+          error: function(param1, param2, param3) {
+            console.log('error');
+            console.log('param 1:', param1);
+            console.log('param 2:', param2);
+            console.log('param 3:', param3);
+          }
+        });
+      },
+
     }
   };
 })();
