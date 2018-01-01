9b9a7be28e701005633d2d79cc2971dbc5cdc41c
Anna Wawrowska <anna.wawrowska.it@gmail.com>
2018-01-01 21:35:43 +0100

    Refactor JS script

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,33 +1,31 @@
 
-$(document).ready(function() {
-  $('.allOffers').slick({
-    centerMode: true,
-    slidesToShow: 3,
-    variableWidth: true,
-    prevArrow: '.slider__arrow--prev',
-    nextArrow: '.slider__arrow--next',
-    swipe: false,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          // arrows: false,
-          centerMode: true,
-          centerPadding: '0',
-          slidesToShow: 1,
-          variableWidth: false,
-          swipe: true,
-        }
-      },
-      // {
-      //   breakpoint: 480,
-      //   settings: {
-      //     arrows: false,
-      //     centerMode: true,
-      //     centerPadding: '40px',
-      //     slidesToShow: 1
-      //   }
-      // }
-    ]
+(function () {
+  $(document).ready(function() {
+    project.sliderInit();
   });
-})
+
+  var project = {
+    sliderInit: function() {
+      $('.allOffers').slick({
+        centerMode: true,
+        slidesToShow: 3,
+        variableWidth: true,
+        prevArrow: '.slider__arrow--prev',
+        nextArrow: '.slider__arrow--next',
+        swipe: false,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              centerMode: true,
+              centerPadding: '0',
+              slidesToShow: 1,
+              variableWidth: false,
+              swipe: true,
+            }
+          },
+        ]
+      });
+    }
+  };
+})();
