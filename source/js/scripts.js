367bc675f422fe656ea29a2a851d1811d55e1662
Anna Wawrowska <anna.wawrowska.it@gmail.com>
2018-01-01 21:21:50 +0100

    Add RWD styles

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -6,26 +6,28 @@ $(document).ready(function() {
     variableWidth: true,
     prevArrow: '.slider__arrow--prev',
     nextArrow: '.slider__arrow--next',
-    swipe: false
-    // responsive: [
-    //   {
-    //     breakpoint: 768,
-    //     settings: {
-    //       arrows: false,
-    //       centerMode: true,
-    //       centerPadding: '40px',
-    //       slidesToShow: 3
-    //     }
-    //   },
-    //   {
-    //     breakpoint: 480,
-    //     settings: {
-    //       arrows: false,
-    //       centerMode: true,
-    //       centerPadding: '40px',
-    //       slidesToShow: 1
-    //     }
-    //   }
-    // ]
+    swipe: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          // arrows: false,
+          centerMode: true,
+          centerPadding: '0',
+          slidesToShow: 1,
+          variableWidth: false,
+          swipe: true,
+        }
+      },
+      // {
+      //   breakpoint: 480,
+      //   settings: {
+      //     arrows: false,
+      //     centerMode: true,
+      //     centerPadding: '40px',
+      //     slidesToShow: 1
+      //   }
+      // }
+    ]
   });
 })
